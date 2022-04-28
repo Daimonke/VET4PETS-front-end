64eb27846ed670b6b80a8c2e73f06a66a914d6f3
Daimonke <Daimonas98@gmail.com>
2022-04-28 15:39:34 +0300

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Suspense, lazy } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Route, Routes } from 'react-router-dom'
 import LoadingPage from './pages/loadingPage'
 
 const MainPage = lazy(() => import("./pages/mainPage"));
@@ -10,12 +10,10 @@ function App() {
 
   return (
     <Suspense fallback={<LoadingPage />}>
-    <Router basename='/VET4PETS-front-end'>
       <Routes>
         <Route path="/" element={ <MainPage /> } />
         <Route path="/*" element={ <Page404 /> } />
       </Routes>
-    </Router>
     </Suspense>
   );
 }
