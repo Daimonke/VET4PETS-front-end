ca6f1210c945028824dc19adf97d2e166e244a2c
Daimonke <Daimonas98@gmail.com>
2022-04-29 11:58:36 +0300

    removed routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
 import { Suspense, lazy } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoadingPage from './pages/loadingPage'
 
 const MainPage = lazy(() => import("./pages/mainPage"));
-const Page404 = lazy(() => import("./pages/page404"));
 
 function App() {
 
   return (
     <Suspense fallback={<LoadingPage />}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/404" element={<Page404 />} />
-        </Routes>
-      </BrowserRouter>
+          <MainPage />
     </Suspense>
   );
 }
