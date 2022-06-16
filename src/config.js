3c99e926206a5de4f7edabd094771cb49db8a805
Daimonke <Daimonas98@gmail.com>
2022-06-16 10:47:15 +0300

    prod api_uri

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API_URI = 'https://vet4pets.herokuapp.com/api/'
-export const API_URI = 'http://localhost:8080/api/'
+export const API_URI = 'https://vet4pets.herokuapp.com/api/'
+// export const API_URI = 'http://localhost:8080/api/'
