fbb53d3c90a5b35cc73976d1cf9fa262728bd6d8
Daimonke <Daimonas98@gmail.com>
2022-06-16 10:40:32 +0300

    Fixes for new DB

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_URI = 'https://vet4pets.herokuapp.com/api/'
+// export const API_URI = 'https://vet4pets.herokuapp.com/api/'
+export const API_URI = 'http://localhost:8080/api/'
--- src/pages/Components/Prescriptions.js
+++ src/pages/Components/Prescriptions.js
@@ -18,16 +18,16 @@ export default function Prescriptions(props) {
   const [loading, setLoading] = useState(true)
   const [refresh, setRefresh] = useState(false)
 
-function handleSearch(value){
-  setShowPrescriptions(logs.filter(log => log.name.includes(value)))
-}
+  function handleSearch(value) {
+    setShowPrescriptions(logs.filter(log => log.name.includes(value)))
+  }
 
   useEffect(() => {
     axios.get(`${API_URI}prescriptions/`)
       .then(function (res) {
         setLogs(res.data)
-        if(showPet) setShowPrescriptions(res.data.filter(item => item.pet_id === showPet))
-        if(!showPet) setShowPrescriptions(res.data)
+        if (showPet) setShowPrescriptions(res.data.filter(item => item.pet_id === showPet))
+        if (!showPet) setShowPrescriptions(res.data)
         setLoading(false)
       })
       .catch(function (error) {
@@ -35,22 +35,23 @@ function handleSearch(value){
       })
   }, [showPet, refresh])
 
+
   return (
     <Container disableGutters={true} maxWidth={false} >
       {loading ? <Loading /> :
-      <div>
-        <Autocomplete
-      disablePortal
-      id="search"
-      onInputChange={(e, value) => handleSearch(value)}
-      options={logs.filter((v,i,a)=>a.findIndex(v2=>(v2.name===v.name))===i)}
-      getOptionLabel={(option) => option.name}
-      sx={{ mt: 2, mb: 2 }}
-      renderInput={(params) => <TextField {...params} label='Search by name' />}
-    />
-      <AddPrescription getPrescriptions={setRefresh}></AddPrescription>
-        {showPrescriptions.length !== 0 ? <VerticalTabs data={showPrescriptions} prescriptions={true}></VerticalTabs> : null}
-      </div>
+        <div>
+          <Autocomplete
+            disablePortal
+            id="search"
+            onInputChange={(e, value) => handleSearch(value)}
+            options={logs.filter((v, i, a) => a.findIndex(v2 => (v2.name === v.name)) === i)}
+            getOptionLabel={(option) => option.name}
+            sx={{ mt: 2, mb: 2 }}
+            renderInput={(params) => <TextField {...params} label='Search by name' />}
+          />
+          <AddPrescription getPrescriptions={setRefresh}></AddPrescription>
+          {showPrescriptions.length !== 0 ? <VerticalTabs data={showPrescriptions} prescriptions={true}></VerticalTabs> : null}
+        </div>
       }
     </Container>
   )
